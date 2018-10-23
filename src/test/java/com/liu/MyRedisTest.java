e39f5106562a773d6177c2fd9cdab06776cc8784
2397459813 <2397459813@qq.com>
2018-10-23 15:30:23 +0800

    develop提交测试类

--- src/test/java/com/liu/MyRedisTest.java
+++ src/test/java/com/liu/MyRedisTest.java
@@ -225,7 +225,7 @@ public class MyRedisTest {
 	
 	@Test
 	public void zrevrange() throws Exception{
-		LOGGER.info("²âÊÔzrevrange------>start");
+		LOGGER.info("²âÊÔzrevrange------======>start");
 		Set<String> result  = service.zrevrange("myzset",1,2);
 		LOGGER.info("²âÊÔzrevrange½á¹û=[{}]",result);
 	}
